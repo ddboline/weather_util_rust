c15f703110053c30d200b183a936452c7e40a27f
Daniel Boline <ddboline@gmail.com>
2020-02-23 10:09:25 -0500

    update deb pkg description

--- scripts/build_deb_docker.sh
+++ scripts/build_deb_docker.sh
@@ -9,6 +9,6 @@ ln -s /usr/bin/python3 /usr/bin/python
 
 cargo build --release
 
-printf "Process and display info about gps activity files\n" > description-pak
+printf "CLI Weather API\n" > description-pak
 echo checkinstall --pkgversion ${VERSION} --pkgrelease ${RELEASE} -y
 checkinstall --pkgversion ${VERSION} --pkgrelease ${RELEASE} -y
