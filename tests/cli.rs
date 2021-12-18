3d4511c2b299d31b343fb4c63d6b538c5df24e6a
Daniel Boline <ddboline@gmail.com>
2021-12-17 23:37:29 -0500

    add zip code args

--- tests/cli.rs
+++ tests/cli.rs
@@ -8,7 +8,9 @@ fn test_default() -> Result<(), Error> {
     let bin = cargo_bin("weather-util-rust");
     assert!(bin.exists());
 
-    let output = Command::cargo_bin("weather-util-rust")?.output()?;
+    let output = Command::cargo_bin("weather-util-rust")?
+        .args(["-z", "10001"])
+        .output()?;
 
     let stdout = String::from_utf8_lossy(&output.stdout);
     let stderr = String::from_utf8_lossy(&output.stderr);
